e77b5b39edba1af8e3d3048576d6a337ef4db0ff
Paul Okoli <paulokoli7@gmail.com>
2023-01-20 19:09:33 +0000

    Add 1-pack_web_static.py file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Fabfile to generates a .tgz archive from the contents of web_static.
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """
+    Targging project directory into a packages as .tgz
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
+        return None
